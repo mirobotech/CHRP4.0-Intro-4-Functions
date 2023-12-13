d63ca0d7a57812a71df4810de8605a28d9ef5620
John Rampelt <johnrampelt@gmail.com>
2023-12-12 21:33:34 -0500

    Update comments and activities
    Update Program Analysis Activities and Programming Activities

--- CHRP4-Intro-4-Functions.X/Intro-4-Functions.c
+++ CHRP4-Intro-4-Functions.X/Intro-4-Functions.c
@@ -1,14 +1,14 @@
 /*==============================================================================
  Project: Intro-4-Functions             Activity: mirobo.tech/intro-4
- Date:    August 11, 2023
+ Date:    December 11, 2023
  
- This introductory programming activity for the mirobo.tech CHRP4 demonstrates
- the use of functions and the ability to pass variables between the program's
- main code and the function code.
+ This introductory programming activity for the mirobo.tech CHRP4 and UBMP4
+ demonstrates the use of functions and the ability to pass variables between
+ the program's main code and the function code.
  
  Additional program analysis and programming activities examine the location of
- function code within the program, introduce function prototypes, and reinforce
- the concepts of global and local variables and making sounds using loops.
+ function code within the program, introduce function prototypes, reinforce the
+ concepts of global variables, local variables, and making sound using loops.
 ==============================================================================*/
 
 #include    "xc.h"              // Microchip XC8 compiler include file
@@ -85,7 +85,7 @@ int main(void)
         // PWM LED4 with current brightness
         pwm_LED4(LED4Brightness);
         
-        // Activate bootloader if SW1 is pressed.
+        // Reset the microcontroller and start the bootloader if SW1 is pressed.
         if(SW1 == 0)
         {
             RESET();
@@ -93,13 +93,23 @@ int main(void)
     }
 }
 
-// Move the function code to here in Program Analysis step 6.
+// Move the function code to here in Program Analysis Activities step 6.
 
 
 /* Learn More -- Program Analysis Activities
  * 
- * 1.   There are three functions in this program. Which function will run
- *      first? How do you know?
+ * 1.   Functions start with a function declaration (the first line of the
+ *      function that provides the return type, function name, and optional
+ *      function parameters). The main function, starting with the function
+ *      declaration 'int main(void)' has been a part of every introductory
+ *      program so far, and it is still a part of this program.
+ * 
+ *      This program also contains two new functions: one begins with the
+ *      function declaration 'unsigned char read_button(void)', and the other
+ *      with 'void pwm_LED4(unsigned char pwmValue)'. 
+
+ *      Where are these functions located in the program? Which function do you
+ *      think will run first? How can you know?
  * 
  * 2.   What is the purpose of the 'unsigned char' variable type declaration in
  *      the read_button() function? Is it used by the function to receive a
@@ -113,15 +123,16 @@ int main(void)
  * 
  * 5.   What is the purpose of the 'unsigned char' variable type declaration in
  *      the pwm_LED4() function? Where does the value of the variable come from?
- *      Which variable in the function stores this value?
+ *      Which variable in the function will be used to store this value?
  * 
  * 6.   C language compilers typically read through an entire program file in a
  *      single pass, converting the C code into machine code. The two functions
  *      read_button() and pwm_LED4() are located above the main() function so
- *      that their memory locations and variables will be defined as they are
- *      compiled, before the rest of the program is compiled. When the compiler
- *      later encounters a function call statement to one of the functions, it
- *      already knows that the functions exist, and where to access their code.
+ *      that their memory locations and variables will be defined when they are
+ *      compiled, before the remainder of the program is compiled. When the
+ *      compiler later encounters a function call statement to one of these
+ *      functions, it already knows that the functions exist, and where and how
+ *      to access their code and variables.
  * 
  *      Try moving the read_button() and pwm_LED4() functions to the end of the
  *      program, below the closing brace of the main() function, and build the
@@ -143,49 +154,50 @@ int main(void)
  * 
  *      Let's explore how this works. Leave the functions in their new location,
  *      below the main() function, and add the two function prototype
- *      statements, below, above the main() function and all the program code.
- *      In other words, add these two lines to the original location of the
- *      functions in the program, right below the variable definitions:
+ *      statements shown below, above the main() function and all of the program
+ *      code. In other words, add these two lines back in the original location
+ *      of the functions in the program, right below the variable definitions:
 
 unsigned char read_button(void);
 
 void pwm_LED4(unsigned char);
 
  *      The function prototype for the pwm_LED4() function is slightly different
- *      than the actual function declaration statement which is now in its new
- *      location, after the while loop in the program code. Can you identify
- *      the difference and, thinking about the linear compilation process
- *      described earlier, provide a reason why the difference is not a real
- *      concern during the compilation?
+ *      than the actual function declaration statement that was moved below the
+ *      while loop in the program code. Can you identify the difference?
+ *      Thinking about the compilation process described earlier, can you
+ *      provide a reason why the difference will not be a problem during
+ *      compilation?
  * 
  *      Building the program now, with the added function prototypes, should
  *      work without generating errors just as it did in the original program.
  *      Try it!
  * 
- * 7.   In the C language functions may be located either above the program code
- *      that calls them, below their function call statements if function
- *      prototypes are supplied before the function call (as seen in 6, above),
- *      or even in completely separate files known as function libraries.
+ * 7.   In the C language, functions can be located either above the main
+ *      program code that calls them (as they were in the original program),
+ *      below the main() function of the program if function prototypes are
+ *      added above the main code (as seen in step 6, above), or in completely
+ *      separate files known as function libraries.
  * 
- *      Function libraries are separate source '.c' files which are typically
- *      joined to your project's main '.c' file using 'include' statements
- *      specifying a '.h' header file. The header files contain both the
- *      variable definitions and the function prototypes for the associated .c
- *      files containing the library function source code, and an typically also
- *      contain comments and descriptions relating to the purpose and use of the
- *      included functions.
+ *      Function libraries are separate '.c' source code files which are
+ *      typically joined to your project's main '.c' file using 'include'
+ *      statements. The include statements specify a '.h' header file. Header
+ *      files contain both the variable definitions and the function prototypes
+ *      for their associated .c files, which contain just the function's C
+ *      language source code. Header files typically also contain comments and
+ *      descriptions relating to the purpose and use of the included functions.
  * 
- *      The statement '#include "UBMP420.h"' near the top of this program (and
+ *      The statement '#include "CHRP4.h"' near the top of this program (and
  *      all of the other Introductory programs), adds external functions into
- *      the main program. The 'UBMP420.h' file contains function prototypes and
- *      other details about the functions located in the 'UBMP420.c' file, along
+ *      the main program. The 'CHRP4.h' file contains function prototypes and
+ *      other details about the functions located in the 'CHRP4.c' file, along
  *      with various symbolic constants used by both the main program in this
- *      source code file, as well as the program code in the UBMP420.c file.
+ *      source code file, as well as the program code in the CHRP4.c file.
  * 
- *      Open the UBMP420.c file in the code editor and you will find two
+ *      Open the CHRP4.c file in the code editor and you will find the two
  *      config() functions called from this program's main() function. Are any
- *      values passed between this code and the two config() functions? How do
- *      you know?
+ *      values passed between this code and the config() functions? How can you
+ *      tell?
  * 
  * 8.   The 'button' variable is a global variable because it was assigned
  *      at the beginning of this program, outside of any functions. Global
@@ -199,48 +211,53 @@ void pwm_LED4(unsigned char);
  *      What would happen to the value of the LED4Brightness variable if it was
  *      used in the pwm_LED4() function?
  * 
+ * 10.  Functions make it easy and quick to use or re-use complex code through
+ *      a simple function call. Take a close look at this program's main()
+ *      function, and read through the code again. What benefits have been
+ *      obtained by using the read_button() and pwm_LED4() calls in the code
+ *      instead of placing the entire contents of the function code into the
+ *      program code?
+ *      
+ *      Likewise, what benefits are provided to beginner programmers by using
+ *      the OSC_config(); and CHRP4_config(); function calls instead of having
+ *      their function code inserted into the main program?
+ * 
+ * 11.  The OSC_config() and CHRP4_config() functions are defined in the CHRP4.h
+ *      header file, and their program code is located in the CHRP4.c file. What
+ *      other functions are available in the header file? Is their corresponding
+ *      program code in also in the CHRP4.c file?
+ *      
+ * 
  * Programming Activities
  * 
- * 1.   It might be useful to have a button that instantly turns LED D4 fully
- *      on or off instead of waiting for it to brighten and dim while the UP
- *      and DOWN buttons are held to change the PWM duty cycle. But, PWM dimming
- *      dimming capability is also a useful feature that should be retained.
+ * 1.   In addition to brightening or dimming an LED using PWM, it would be
+ *      useful to have buttons that are able to instantly turn LED D4 fully on
+ *      or fully off. But, PWM dimming is still a useful capability that should
+ *      be retained.
  * 
- *      Modify the read_button() and main() functions to use SW2 as an
+ *      Modify the read_button() and main() functions to enable SW2 as an
  *      instant-on button, and SW5 as an instant-off button. Pressing either of
- *      these buttons should over-write the current LED4Brightness value with
- *      either 255 or 0, while still allowing SW3 and SW4 to adjust the
- *      brightness smoothly and in smaller increments when pressed.
+ *      these buttons should over-write the current LED4Brightness value, while
+ *      still allowing SW3 and SW4 to smoothly adjust the brightness using PWM.
  *
- * 2.   Create a new program that uses functions to read the pushbuttons and to
- *      control the LEDs. One LED will light corresponding to each pushbutton
- *      while the button is being pressed, and all of the LEDs should turn off
- *      when the buttons are released.
+ * 2.   Duplicate this program to create a new program using one function to
+ *      read the pushbuttons, and a second function to control the LEDs. Each
+ *      pushbutton should light its corresponding LED when pressed, and turn
+ *      the LED off when it is released.
  * 
- *      Start by creating a function that will return a number (from 1-4)
- *      corresponding to which of the SW2 to SW5 pushbuttons is being pressed,
- *      or that returns a value of 0 if no buttons are pressed. Then, create a
- *      second function that will accept a number from 0 to 4 which will turn
- *      off all of the LEDs in response to a 0 input, or light the appropriate
- *      LED for each of the input numbers from 1-4.
-  * 
- * 3.   Create a four note music player using two functions, one to read the
- *      pushbuttons (similar to Activity 2, above), and a second to play a
- *      corresponding sound on the piezo beeper using a loop.
- * 
- *      Start by creating a sound function that will receive a parameter
- *      representing a tone's period or pitch. Then, modify the pushbutton
- *      function to return a different period or pitch in response to each
- *      button being pressed. You could even modify your pushbutton function to
- *      respond to multiple, simultaneous button presses so that your program
- *      is able to play more than four notes while using just four buttons!
+ * 3.   Duplicate your program from step 2 to create a four note (or more)
+ *      music player using two functions: one to read the pushbuttons, and a
+ *      second to play a corresponding sound on the piezo beeper using a loop
+ *      to create the sound wave's period. Try to do this by creating a single
+ *      sound function that will accept a parameter representing the note's
+ *      pitch, and have each button pass the sound function a different value.
  * 
  * 4.   A function that converts an 8-bit binary value into its equivalent
- *      3-digit decimal number might be useful for helping us to debug our
- *      programs (as you will see in the next activity). Create a function that
- *      converts an 8-bit binary number into three decimal character variables
- *      representing the hundreds, tens, and ones digits of a (binary) number
- *      passed to it. For example, passing the function the value of 142 will
+ *      3-digit decimal numbers might be useful for helping to debug programs
+ *      (and we will need one in the next activity). Create a function that
+ *      converts an 8-bit binary number into three character variables to store
+ *      the hundreds, tens, and ones digits of a (binary) number passed to the
+ *      function. For example, passing the function the value of 142 will
  *      result in the hundreds variable containing the value 1, the tens
  *      variable containing 4, and the ones variable 2. How could you test this
  *      function to verify that it works? Try it! (Hint: the BCD number system
